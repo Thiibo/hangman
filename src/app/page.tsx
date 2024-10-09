671051c34d3802a5bee9b41b0dad708c31a7dfa1
Thiibo <40576021+Thiibo@users.noreply.github.com>
2024-10-09 22:41:29 +0200

    Make Home component use Game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,14 @@
-import Image from "next/image";
-import { HangmanGame } from "./modules/game";
+import { Game } from "./components/Game";
 
 export default function Home() {
-  const game = new HangmanGame();
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <WordStateDisplay game={game} />
+      <main className="h-full">
+        <Game />
       </main>
       <footer className="row-start-3 flex gap-2 flex-wrap items-center justify-center">
         Made by <a className="text-orange-600" href="https://github.com/Thiibo">Thiibo</a>
       </footer>
     </div>
   );
-}
-
-function WordStateDisplay({ game } : { game: HangmanGame }) {
-  const letters = game.currentWordStatus.map(letter => (
-    <div className="p-6 h-1 w-1 text-6xl border-b-4 white">{letter}</div>
-  ));
-
-  return (
-    <div className="row-start-3 flex gap-10 flex-wrap items-center justify-center">
-      {letters}
-    </div>
-  )
 }
